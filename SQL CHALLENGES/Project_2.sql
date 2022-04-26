57274601a166b100f45fbce2a577897eedbf4f39
Lakshmi Prasanna Bogineni <103848742+prasannabogineni@users.noreply.github.com>
2022-04-26 20:57:17 +0530

    Update and rename Project 2 to Project_2.sql

--- SQL CHALLENGES/Project_2.sql
+++ SQL CHALLENGES/Project_2.sql
@@ -1,6 +1,6 @@
-                                                    Design a store database
+ /*                                                   Design a store database
 
-Create your own store! Your store should sell one type of things, like clothing or bikes, whatever you want your store to specialize in. You should have a table for all the items in your store, and at least 5 columns for the kind of data you think you'd need to store. You should sell at least 15 items, and use select statements to order your items by price and show at least one statistic about the items.
+Create your own store! Your store should sell one type of things, like clothing or bikes, whatever you want your store to specialize in. You should have a table for all the items in your store, and at least 5 columns for the kind of data you think you'd need to store. You should sell at least 15 items, and use select statements to order your items by price and show at least one statistic about the items.*/
 
 CREATE TABLE mystore(id INTEGER PRIMARY KEY, item_model TEXT,color TEXT,price INTEGER, selling_quantity INTEGER);
 INSERT INTO mystore values(1, "jeans", "Blue", 1000, 2);
@@ -12,5 +12,5 @@ INSERT INTO mystore values(6, "T-shirt", "Yellow", 500, 10);
 INSERT INTO mystore values(7, "Kurtas", "Brown", 700, 3);
 
 select * from mystore where price > 1500 order by price;
-select selling_quantity, sum(price) from mystore group by selling_quantity
+select selling_quantity, sum(price) from mystore group by selling_quantity;
 
