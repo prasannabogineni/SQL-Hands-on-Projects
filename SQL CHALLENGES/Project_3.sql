6a0f4c605f4102f4df79fe76dd1f843952bf0fe9
Lakshmi Prasanna Bogineni <103848742+prasannabogineni@users.noreply.github.com>
2022-04-26 20:58:02 +0530

    Update and rename Project 3 to Project_3.sql

--- SQL CHALLENGES/Project_3.sql
+++ SQL CHALLENGES/Project_3.sql
@@ -1,10 +1,10 @@
-                                                                Challenge: Karaoke song selector
+/*                                                                Challenge: Karaoke song selector
   Step 1
 Ever sung karaoke? It's a place where you sing songs with your friends, and it's a lot of fun. We've created a table with songs, and in this challenge, you'll use queries to decide what songs to sing. For the first step, select all the song titles.
 Step 2
 Maybe your friends only like singing either recent songs or truly epic songs. Add another SELECT that uses OR to show the titles of the songs that have an 'epic' mood or a release date after 1990.
 People get picky at the end of the night. Add another SELECT that uses AND to show the titles of songs that are 'epic', and released after 1990, and less than 4 minutes long.
-Note that the duration column is measured in seconds.
+Note that the duration column is measured in seconds.*/
 
 
 
